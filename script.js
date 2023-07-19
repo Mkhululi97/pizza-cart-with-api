a452c85cebc8b50d5dcedd797602847e7558f179
mkhululi <mkhululimtshali@gmail.com>
2023-07-19 09:56:23 +0200

    make buy update the cart and display the update cart, without refreshing the page.

--- script.js
+++ script.js
@@ -3,11 +3,42 @@ document.addEventListener("alpine:init", () => {
     return {
       title: "Pizza Cart",
       pizzas: [],
+      username: "Mkhululi97",
+      cartId: "g9Q90A1Lk4",
+      cartPizzas: [],
+      cartTotal: 0.0,
+      getCart() {
+        const getCartUrl = `https://pizza-api.projectcodex.net/api/pizza-cart/${this.cartId}/get`;
+        return axios.get(getCartUrl);
+      },
+      addPizza(pizzaId) {
+        return axios.post(
+          "https://pizza-api.projectcodex.net/api/pizza-cart/add",
+          {
+            cart_code: this.cartId,
+            pizza_id: pizzaId,
+          }
+        );
+      },
+      showCartData() {
+        this.getCart().then((result) => {
+          let cartData = result.data;
+          this.cartPizzas = cartData.pizzas;
+          this.cartTotal = cartData.total;
+        });
+      },
       init() {
+        /* GET ALL THE PIZZAS FROM THE API */
         axios
           .get("https://pizza-api.projectcodex.net/api/pizzas")
           .then((result) => (this.pizzas = result.data.pizzas));
+        /* SHOW ALL THE PIZZAS IN THE CART CURRENTLY */
+        this.showCartData();
       },
+      addPizzaToCart(pizzaId) {
+        this.addPizza(pizzaId).then(this.showCartData());
+      },
+      // removePizzaFromCart()
     };
   });
 });
